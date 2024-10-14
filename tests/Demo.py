cd303cb308edd4da22ccbfb9e68a316d0dd3ab47
Rui <rui.carlos.andrade.martins@gmail.com>
2024-10-14 16:35:39 +0100

    Changed the read_data class constructor to receive as input the folder where the "Current.txt" and "Voltage.txt" files are located. The class will read the .txt files and load the matrices.
    Also changed the dBar class constructor to receive the scatteringType string, the k-grid, the data and parameters for the z-grid. The constructor now handles the creation of the scattering object.
    Added a plt.show() to the to dBar.plot()

--- tests/Demo.py
+++ tests/Demo.py
@@ -1,20 +1,33 @@
+import numpy as np
 import py_dbar as pd
 from py_dbar import read_data,k_grid, dBar 
 
+#IMPORTANT: Make sure the current working directory is tests/..
+
+# Parameters
+L = 16                  #Number of electrodes
+R = 1                   #Tank radius
+A = 1                   #Electrode area
+
+
 # Read the boundary measurements and create the Dirichlet-to-Neumann map
+# Assuming the cwd is .../pydbar
+parentFolderNow = "tests/EIT_Data/Object1"      #local path to data relative to cwd 
+parentFolderHomo = "tests/EIT_Data/ObjectH"     #local path to data relative to cwd 
+
 # Frame to determine the conductivity: 
-Now = read_data.read_data("Object1/", 1, 1, 16)
+Now = read_data.read_data(parentFolderNow, R, A, L)
 # Frame of Reference - Homogeneous
-Ref = read_data.read_data("ObjectH/", 1, 1, 16)
+Ref = read_data.read_data(parentFolderHomo, R, A, L)
 
 # Establish a k_grid which corresponds to a spectral parameter
 Kp = k_grid.k_grid(4, 6)
 
 # Creates the model which corresponds to the Dbar equation in the spectral parameter at each z of a z_grid.
-model = dBar.dBar(Kp, Now, Ref, 1., 6)
+model = dBar.dBar("partial", Kp, Now, Ref, 1., 5)
 
 # Solves the problem for each z in the 2D plane, hence determining the conductivity at each z
-model.solve(Kp)
+model.solve()
 
 # Plots the solution
 model.plot()
